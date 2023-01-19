5a6f21511f6580b3d2a85ca769d9c7e4a7961a70
Youngjung3 <youngj5396@hs.ac.kr>
2023-01-19 14:33:42 +0900

    01-19 Load

---
+++ js_jq/ajax/load-jq.js
@@ -0,0 +1,4 @@
+$(function(){
+    $("#newsWrap_1").load('./01-news.html #news_3');
+    $("#newsWrap_2").load('./01-news.html #news_2');
+})
---
+++ js_jq/ajax/load2.js
@@ -0,0 +1,21 @@
+addEventListener("DOMContentLoaded", () => {
+    //load.html 요소를 변수에 할당
+    let book1=document.querySelector("#book1");
+    let book2=document.querySelector("#book2");
+    // 데이터파일의 경로를 변수에 할당
+    const url="./book.html";
+    // XMLHttpRequest 객체를 생성
+    let request=new XMLHttpRequest();
+    // 서버에 요청프로그램작성
+    request.open("GET",url);
+    request.responseType="document"
+
+    request.onload=()=>{
+        let response = request.response;
+        let ids=response.querySelectorAll("[id]");
+        let outer='';
+        book1.innerHTML=ids[0].innerText;
+        book2.innerHTML=ids[1].innerText;
+    }
+    request.send();
+  })
